5ac2a8316b274fcd88db1565ecd22241eb5ef650
Barada32 <barada.90@yandex.ru>
2023-04-25 19:03:00 +0300

    11

---
+++ Arifmetic/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arifmetic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
