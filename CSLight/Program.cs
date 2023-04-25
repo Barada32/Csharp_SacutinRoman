cdcc5bb89cbcea277454df4aab998d0644dbf6ec
Barada32 <barada.90@yandex.ru>
2023-04-25 19:50:54 +0300

    11

--- CSLight/Program.cs
+++ CSLight/Program.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿//#define HI
+//#define INTERPOLATION
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Serialization;
-
 
 namespace CSLight
 {
@@ -12,7 +13,24 @@ namespace CSLight
     {
         static void Main(string[] args)
         {
-          
+
+
+#if HI
+            string greeting = "Привет";
+            string name = "Валентин";
+            string message = greeting+" омлет " + name;
+            Console.WriteLine(message+", рад тебя видеть!");   
+#endif
+
+
+#if INTERPOLATION
+            int age = 27;
+            string name = "Valentin";
+            Console.WriteLine("Привет тебе исполнится " + (age + 1));
+            Console.WriteLine("Ваше имя " + name + " вам " + age + " лет ");
+            Console.WriteLine($"Ваше имя {name} вам {age} лет"); 
+#endif
+
         }
     }
 }
