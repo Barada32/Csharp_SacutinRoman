f08e57e0e53ca8bfd6f7dcacd568685f6e1353bf
barada32 <barada.9012@gmail.com>
2023-05-12 14:13:27 +0300

    120-240

--- CSLight/Program.cs
+++ CSLight/Program.cs
@@ -16,7 +16,23 @@ namespace CSLight
             Console.InputEncoding = Encoding.Unicode;
             Console.OutputEncoding = Encoding.Unicode;
 
-    
+            float money;
+            int years;
+            int percent;
+            Console.WriteLine("Введите количество денег, внесенных на вклад: ");
+            money = Convert.ToSingle(Console.ReadLine());
+            Console.Write("На сколько лет открыт вклад: ");
+            years = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Под какой процент?");
+            percent = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < years; i++)
+            {
+                money += money / 100 * percent;
+                Console.WriteLine("Через "+ (i+1) + "году у вас " + money);
+                Console.ReadKey();
+            }
+
+
 
         }
     }
