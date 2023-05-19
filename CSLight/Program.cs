7a7617db839b4e67f13d46a8c19e58b827d27733
barada32 <barada.9012@gmail.com>
2023-05-19 18:18:21 +0300

    4;11

--- CSLight/Program.cs
+++ CSLight/Program.cs
@@ -15,8 +15,81 @@ namespace CSLight
         {
             Console.InputEncoding = Encoding.Unicode;
             Console.OutputEncoding = Encoding.Unicode;
+            Console.CursorVisible = false;
+            char[,] map =
+            {
+                { '#','#','#','#','#','#','#','#','#','#','#','#'},
+                { '#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#'},
+                { '#','#','#','#',' ',' ',' ',' ',' ',' ',' ','#'},
+                { '#',' ',' ','#',' ',' ',' ',' ',' ',' ',' ','#'},
+                { '#',' ',' ','#',' ',' ',' ',' ',' ',' ',' ','#'},
+                { '#',' ',' ','#',' ',' ',' ',' ',' ',' ',' ','#'},
+                { '#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#'},
+                { '#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#'},
+                { '#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#'},
+                { '#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#'},
+                { '#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#'},
+                { '#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#'},
+                { '#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#'},
+                { '#','#','#','#','#','#','#','#','#','#','#','#'}
 
-    
+
+            };
+            int userX = 6; int userY = 6;
+
+
+            while (true)
+            {
+                Console.SetCursorPosition(0, 0);
+                for (int i = 0; i < map.GetLength(0); i++)
+                {
+                    for (int j = 0; j < map.GetLength(1); j++)
+                    {
+
+                        Console.Write(map[i, j]);
+
+                    }
+                    Console.WriteLine();
+                }
+                Console.SetCursorPosition(userY, userX);
+                Console.Write('@');
+                ConsoleKeyInfo charKey = Console.ReadKey();
+                switch (charKey.Key)
+                {
+                    case ConsoleKey.W:
+                        if (map[userX-1,userY]!='#')
+                        {
+                            userX--;
+                        }
+                        break;
+                    case ConsoleKey.S:
+                        if (map[userX + 1, userY] != '#')
+                        {
+                            userX++;
+                        }
+                        break;
+                    case ConsoleKey.D:
+                        if (map[userX, userY + 1] != '#')
+                        {
+                            userY++;
+                        }
+                        break;
+                    case ConsoleKey.A:
+                        if (map[userX , userY - 1] != '#')
+                        {
+                            userY--;
+                        }
+                        break;
+
+
+                }
+
+
+                
+                Console.Clear();
+
+            }
+
 
         }
     }
