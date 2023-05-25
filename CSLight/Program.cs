a2437292579fd4d6d3185a812033ae0fb6801956
barada32 <barada.9012@gmail.com>
2023-05-25 15:54:47 +0300

    //4:54

--- CSLight/Program.cs
+++ CSLight/Program.cs
@@ -4,6 +4,7 @@
 //#define REF_OUT_4
 //#define DYNAMIC_ARRAY_5
 //#define RESIZE_ARRAY_OVERLOADING_6
+#define HEALTH_MANA_BAR_7
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -222,7 +223,47 @@ namespace CSLight
 
 #endif
 
-            //4:48
+#if HEALTH_MANA_BAR_7
+            int health = 12, maxHealth = 18;
+            int mana = 9, maxMana = 12;
+            while (true)
+            {
+                DrawBar(health, maxHealth, ConsoleColor.Green, 0, '|');
+                DrawBar(mana, maxMana, ConsoleColor.Blue, 1);
+
+                Console.SetCursorPosition(0, 5);    
+                Console.Write("Введите число на которое измениться жизни:");
+                health += Convert.ToInt32(Console.ReadLine());
+                Console.Write("Введите число на которое измениться маны:");
+                mana += Convert.ToInt32(Console.ReadLine());
+                Console.ReadKey();
+                Console.Clear();
+            }
+
+        }
+        static void DrawBar(int value, int maxValue, ConsoleColor color, int position, char symbol = '_')
+        {
+            ConsoleColor defaultColor = Console.BackgroundColor;
+            string bar = "";
+            for (int i = 0; i < value; i++)
+            {
+                bar += symbol;
+            }
+            Console.SetCursorPosition(0, position);
+            Console.Write('[');
+            Console.BackgroundColor = color;
+            Console.Write(bar);
+            Console.BackgroundColor = defaultColor;
+            bar = "";
+            for (int i = value; i < maxValue; i++)
+            {
+                bar += " ";
+            }
+            Console.Write(bar + ']');
+        }
+
+
+#endif
 
 
     }
