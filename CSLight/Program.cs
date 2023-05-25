2e20503256847412c21263458fccb4369fe8e575
barada32 <barada.9012@gmail.com>
2023-05-25 13:30:50 +0300

    //4:48

--- CSLight/Program.cs
+++ CSLight/Program.cs
@@ -3,6 +3,7 @@
 //#define FUNCTIONS_3
 //#define REF_OUT_4
 //#define DYNAMIC_ARRAY_5
+//#define RESIZE_ARRAY_OVERLOADING_6
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -184,6 +185,45 @@ namespace CSLight
         } 
 #endif
 
-        }  
+
+#if RESIZE_ARRAY_OVERLOADING_6
+
+            int[] array1 = new int[5];
+            int[,] array2 = new int[5, 5];
+            array1 = Resize(array1, 6);
+            array2 = Resize(array2, 10, 11);
+            Console.WriteLine(array1.Length);
+            Console.WriteLine(array2.Length);
+        }
+
+        static int[] Resize(int[] array, int size)
+        {
+            int[] tempArray = new int[size];
+            for (int i = 0; i < array.Length; i++)
+            {
+                tempArray[i] = array[i];
+
+            }
+            array = tempArray;
+            return array;
+        }
+        static int[,] Resize(int[,] array, int x, int y)
+        {
+            int[,] tempArray = new int[x, y];
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                { tempArray[i, j] = array[i, j]; }
+
+            }
+            array = tempArray;
+            return array;
+        }
+
+#endif
+
+            //4:48
+
+
     }
 }
