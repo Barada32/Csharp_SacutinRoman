e07885083c95a1b53e793a1f42838552f44975ba
barada32 <barada.9012@gmail.com>
2023-08-03 13:38:44 +0300

    5_45

--- CSLight/Program.cs
+++ CSLight/Program.cs
@@ -1,11 +1,17 @@
-﻿
+﻿//#define COLLECTION_LIST_1
+//#define QUEUE_1
+#define STACK_1
+
+
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Serialization;
+
 
 namespace CSLight
 {
@@ -15,8 +21,56 @@ namespace CSLight
         {
             Console.InputEncoding = Encoding.Unicode;
             Console.OutputEncoding = Encoding.Unicode;
+#if COLLECTION_LIST_1
+            List<int> numbers = new List<int>();
+            numbers.Add(12);
+            numbers.Add(11);
+            numbers.Add(13);
+            numbers.Add(14);
+            numbers.AddRange(new int[] {3,4,5,6});
+            numbers.RemoveAt(3);
+            numbers.Remove(3);
+            numbers.Insert(3, 4);
+         
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            } 
+
+            Console.WriteLine("Число 11 находится на позиции "+(1+ numbers.IndexOf(11)));
+            numbers.Clear();
+#endif
+
+#if QUEUE_1
+
+            Queue<string> patients = new Queue<string>();
+            patients.Enqueue("Василий");
+            patients.Enqueue("Саша");
+            patients.Enqueue("Рома");
+            patients.Enqueue("ОЛЕША");
+
+            Console.WriteLine("Сейчас на прием идет " + patients.Dequeue());
+            Console.WriteLine("Осталитсь в очереди");
+            foreach (var item in patients)
+            {
+                Console.WriteLine(item);
+            }
 
 
+#endif
+#if STACK_1
+Stack<int> stack = new Stack<int>();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+            stack.Push(5);
+
+            foreach (var i in stack) {Console.WriteLine(stack); }
+            
+
+            while (stack.Count > 0) { Console.WriteLine("следующее число в стеке "+stack.Pop()); }
+#endif
 
         }
     }
